ce73ce51965d7669a16390b60c91ddb8765a9c5f
rkamere20 <richardkamere20@gmail.com>
2020-04-13 18:45:37 +0300

    version management
    version 0.0.2
    Added static folder

--- larntech/urls.py
+++ larntech/urls.py
@@ -14,22 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path,include
-
-from tutorials import views
-
 from django.conf.urls import url
 from rest_framework import permissions,routers
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib import admin
+from django.urls import path, include
+
+from tutorials import views
+from tutorials.views import CustomObtainAuthToken
 
 schema_view = get_schema_view(
    openapi.Info(
       title="Tutorials API",
       default_version='v1',
       description="API Description",
-      contact=openapi.Contact(email="richard@larntech.net"),
+      contact=openapi.Contact(email="richard@larntech.com"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
@@ -43,7 +43,7 @@ routers.register(r'users', views.UserViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
    # url(r'^authenticate/', CustomObtainAuthToken.as_view()),
-   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('api/', include(routers.urls)),
+   url('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('', include(routers.urls)),
 
 ]
