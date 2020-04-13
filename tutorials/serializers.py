499726e888f66b37afe7acd91f8d4844cb695847
rkamere20 <richardkamere20@gmail.com>
2020-04-13 18:30:27 +0300

    version management
    version 0.0.2
    Added static folder

--- tutorials/serializers.py
+++ tutorials/serializers.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.models import User
-from rest_framework import serializers
+from rest_framework import serializers, response, status
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     date_joined = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     last_login = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
 
     class Meta:
         model = User
-        fields = ['url', 'username', 'first_name', 'last_name', 'email', 'is_active', 'date_joined', 'last_login']
-
+        fields = ['id', 'url', 'username', 'first_name', 'last_name', 'email', 'is_active', 'date_joined', 'last_login']
